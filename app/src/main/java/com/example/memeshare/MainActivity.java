a4f8e3aa01a415000008adb45e07be7e05582e6b
Sumanjeet Pathak <pathaksumanjeet08@gmail.com>
2021-11-16 13:44:09 +0530

    Second Commit

--- app/src/main/java/com/example/memeshare/MainActivity.java
+++ app/src/main/java/com/example/memeshare/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -13,26 +14,33 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        imageView = findViewById(R.id.imageView);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        loadMeme();
     }
-    private void loadMeme(View view){
+    private void loadMeme(){
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
         String url ="https://meme-api.herokuapp.com/gimme";
 
         // Request a string response from the provided URL.
          JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url,null,
-                new Response.Listener() {
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(Object response) {
-                        String url = response.getClass("url");
+                    public void onResponse(JSONObject response) {
+                        String url = response.optString("url");
+
+                        Glide.with(getApplicationContext()).load(url).into(imageView);
 
                     }
 
@@ -48,8 +56,10 @@ public class MainActivity extends AppCompatActivity {
         queue.add(jsonObjectRequest);
     }
     public void shareMeme(View view) {
+
     }
 
     public void nextMeme(View view) {
+        loadMeme();
     }
 }
